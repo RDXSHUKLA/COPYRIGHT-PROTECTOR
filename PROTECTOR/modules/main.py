81e2f8803d53b91af45700a137a32ca445b1aa5b
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-11-16 17:04:48 +0530

    Update main.py

--- PROTECTOR/modules/main.py
+++ PROTECTOR/modules/main.py
@@ -3,88 +3,58 @@ import os
 import platform
 import psutil
 import time
-import random
-from pyrogram import Client, filters
-from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, Message
+import json
+import asyncio
+from pyrogram import *
 
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, Message
 from config import BOT_USERNAME, OWNER_ID
 from PROTECTOR import PROTECTOR as app
 from config import *
+
 # Constants
-FORBIDDEN_KEYWORDS = ["porn", "xxx", "NCERT", "ncert", "ans", "Pre-Medical", "kinematics", "Experiments", "Experiment", "experiment", "experimens", "XII", "page", "Ans", "meiotic", "divisions", "System.in", "Scanner", "void", "nextInt", "JEE", "ALLEN", "NEET", "jee", "neet", "ans"]
-START_TEXT = """**╭────── ˹ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ˼────⏤͟͟͞͞★**\n**┆◍ ʜᴇʏ, ɪ ᴀᴍ : [𝐆ʀᴏᴜᴘ ꭙ 𝐒ʜɪᴇʟᴅ](https://t.me/GroupXshieldBot) **\n**┆● ᴡᴇ ᴇɴsᴜʀᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ sᴇᴄᴜʀɪᴛʏ💻 !\n**┊● ᴛʜɪs ʙᴏᴛ ᴄᴀɴ ʀᴇᴍᴏᴠᴇ ʟᴏɴɢ ᴛᴇxᴛ**\n**╰─────────────────────────**\n**──────────────────────────**\n**❖ ᴛʜɪs ʙᴏᴛ ᴄᴀɴ ʀᴇᴍᴏᴠᴇ ᴇᴅɪᴛᴇᴅ ᴍsɢs **\n**──────────────────────────**\n**❖ ᴛʜɪs ʙᴏᴛ ᴄᴀɴ ʀᴇᴍᴏᴠᴇ ᴄᴏᴘʏʀɪɢʜᴛ ᴍᴀᴛᴇʀɪᴀʟ...!**\n**──────────────────────────**\n**❖ ᴊᴜsᴛ ᴀᴅᴅ ʙᴏᴛ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴀɴᴅ ᴍᴀᴋᴇ ᴀᴅᴍɪɴ !!**\n**──────────────────────────**\n**❖ Uᴘᴅᴀᴛᴇ ⏤͟͟͞͞  [❖ ∣ Tʜᴇ sᴛʀᴀɴɢᴇʀ ∣ ❖](https://t.me/StrangerAssociation) **\n**──────────────────────────**"""
+START_TEXT = """<b>🤖 ᴄᴏᴘʏʀɪɢʜᴛ ᴘʀᴏᴛᴇᴄᴛᴏʀ 🛡️</b>
 
-SHUKLA = [
-    "https://graph.org/file/9bba2b7ee9ba3806de65d.jpg",
-    "https://graph.org/file/ef82f289043a4fa74f8ff.jpg",
-    "https://graph.org/file/9c27c68958e06ae074c38.jpg",
-    "https://graph.org/file/0ff325b1d2efe80299aa3.jpg",
-    "https://graph.org/file/41167b953cf3579853d47.jpg",
-    "https://graph.org/file/bd93ab42e69305f274028.jpg",
-    "https://graph.org/file/97575db5586c05d6b2898.jpg",
-    "https://graph.org/file/07c393fdf931a407c9bc0.jpg",
-    "https://graph.org/file/f332767490ad3a5ca20e8.jpg",
-    "https://graph.org/file/f3449e9069667f647d14e.jpg",
-    "https://graph.org/file/9f51cdc739f907cbd2c7e.jpg",
-    "https://telegra.ph/file/d7a6a923c38e051ce35f3.jpg",
-    "https://graph.org/file/f86b71018196c5cfe7344.jpg",
-    "https://graph.org/file/a3db9af88f25bb1b99325.jpg",
-    "https://graph.org/file/5b344a55f3d5199b63fa5.jpg",
-    "https://graph.org/file/84de4b440300297a8ecb3.jpg",
-    "https://graph.org/file/84e84ff778b045879d24f.jpg",
-    "https://graph.org/file/a4a8f0e5c0e6b18249ffc.jpg",
-    "https://graph.org/file/ed92cada78099c9c3a4f7.jpg",
-    "https://graph.org/file/d6360613d0fa7a9d2f90b.jpg",
-    "https://graph.org/file/37248e7bdff70c662a702.jpg",
-    "https://graph.org/file/0bfe29d15e918917d1305.jpg",
-    "https://graph.org/file/16b1a2828cc507f8048bd.jpg",
-    "https://graph.org/file/e6b01f23f2871e128dad8.jpg",
-    "https://graph.org/file/cacbdddee77784d9ed2b7.jpg",
-    "https://graph.org/file/ddc5d6ec1c33276507b19.jpg",
-    "https://graph.org/file/39d7277189360d2c85b62.jpg",
-    "https://graph.org/file/5846b9214eaf12c3ed100.jpg",
-    "https://graph.org/file/ad4f9beb4d526e6615e18.jpg",
-    "https://graph.org/file/3514efaabe774e4f181f2.jpg",    
-]
+ʜᴇʏ ᴛʜɪs ɪs ᴄᴏᴘʏʀɪɢʜᴛ ᴘʀᴏᴛᴇᴄᴛᴏʀ ʀᴏʙᴏᴛ 🤖\n
+ᴡᴇ ᴇɴsᴜʀᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ's sᴇᴄᴜʀɪᴛʏ 📌\n
+ᴛʜɪs ʙᴏᴛ ᴄᴀɴ ʀᴇᴍᴏᴠᴇ ʟᴏɴɢ ᴇᴅɪᴛᴇᴅ ᴛᴇxᴛs ᴀɴᴅ ᴄᴏᴘʏʀɪɢʜᴛᴇᴅ ᴍᴀᴛᴇʀɪᴀʟ 📁\n
+ᴊᴜsᴛ ᴀᴅᴅ ᴛʜɪs ʙᴏᴛ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴀɴᴅ ᴍᴀᴋᴇ ɪᴛ ᴀɴ ᴀᴅᴍɪɴ\n
+ғᴇᴇʟ ғʀᴇᴇ ғʀᴏᴍ ᴀɴʏ ᴛʏᴘᴇ ᴏғ **ᴄᴏᴘʏʀɪɢʜᴛ** 🛡️
+"""
+AUTHORIZED_USERS_FILE = "authorized_users.json"
+MAX_MESSAGE_LENGTH = 40
+Devs = ["7044783841", "7019293589", "6757745933"]
 
-##---------------------------------------------------------------------------------
-@app.on_message(filters.command("start"))
-async def start_command_handler(_, msg):
-    buttons = [
-        [InlineKeyboardButton("▪️ ᴀᴅᴅ ᴍᴇ  ▪️", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
-        [InlineKeyboardButton("▪️ ʜᴀɴᴅʟᴇʀ ▪️", callback_data="vip_back")]
-        
-    ]
-    reply_markup = InlineKeyboardMarkup(buttons)
-    await msg.reply_photo(
-        random.choice(SHUKLA),
-        caption=START_TEXT,
-        reply_markup=reply_markup
-    )
+# Initialize a set of bad words
+bad_words = {"xxx"}
 
-# Callback Query Handler
+# Define gd_buttons
 gd_buttons = [
-    [InlineKeyboardButton("▪️ ᴏᴡɴᴇʀ ▪️", url=f"https://t.me/SHIVANSHDEVS"),
-     InlineKeyboardButton("▪️ ʙᴀᴄᴋ ▪", callback_data="back_to_start"),
-     InlineKeyboardButton(" ▪️sᴜᴘᴘᴏʀᴛ ▪️", url="https://t.me/MASTIWITHFRIENDSXD")]
+    [InlineKeyboardButton("ᴏᴡɴᴇʀ", url="https://t.me/JARVIS_V2"),
+     InlineKeyboardButton("• ʙᴀᴄᴋ •", callback_data="back_to_start"),
+     InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/JARVIS_V_SUPPORT")]
 ]
 
-@app.on_callback_query(filters.regex("vip_back"))
-async def vip_back_callback_handler(_, query: CallbackQuery):
-    await query.message.edit_caption(caption=START_TEXT, reply_markup=InlineKeyboardMarkup(gd_buttons))
+# Load authorized users from file
+def load_authorized_users():
+    if os.path.exists(AUTHORIZED_USERS_FILE):
+        with open(AUTHORIZED_USERS_FILE, "r") as f:
+            return json.load(f)
+    return [OWNER_ID]
 
-@app.on_callback_query(filters.regex("back_to_start"))
-async def back_to_start_callback_handler(_, query: CallbackQuery):
-    await query.answer()
-    await query.message.delete()
-    await start_command_handler(_, query.message)
+# Save authorized users to file
+def save_authorized_users(users):
+    with open(AUTHORIZED_USERS_FILE, "w") as f:
+        json.dump(users, f)
 
+AUTHORIZED_USERS = load_authorized_users()
 
-##---------------------------------------------------------------------------------
 # Bot Functionality
-
 start_time = time.time()
 
+# Utility functions
 def time_formatter(milliseconds: float) -> str:
     seconds, milliseconds = divmod(milliseconds, 1000)
     minutes, seconds = divmod(seconds, 60)
@@ -98,78 +68,112 @@ def size_formatter(bytes: int) -> str:
         bytes /= 1024.0
     return f"{bytes:.2f} {unit}"
 
+# Command Handlers
+@app.on_message(filters.command("start"))
+async def start_command_handler(_, msg):
+    buttons = [
+        [InlineKeyboardButton("ᴀᴅᴅ ᴍᴇ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+        [InlineKeyboardButton("• ʜᴀɴᴅʟᴇʀ •", callback_data="vip_back")]
+    ]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    await msg.reply_photo(
+        photo="https://telegra.ph/file/8f6b2cc26b522a252b16a.jpg",
+        caption=START_TEXT,
+        reply_markup=reply_markup
+    )
+
 @app.on_message(filters.command("ping"))
 async def activevc(_, message: Message):
     uptime = time_formatter((time.time() - start_time) * 1000)
     cpu = psutil.cpu_percent()
     storage = psutil.disk_usage('/')
-
     python_version = platform.python_version()
 
     reply_text = (
-        f"❖ ᴄᴏᴘʏʀɪɢʜᴛ ʙᴏᴛ ᴘɪɴɢ sᴛᴀᴛs ⏤͟͟͞͞★\n\n"
-        f"● ᴜᴘᴛɪᴍᴇ ➥ {uptime}\n"
-        f"● ᴄᴘᴜ ➥ {cpu}%\n"
-        f"● ᴛᴏᴛᴇʟ ꜱᴛᴏʀᴀɢᴇ ➥ {size_formatter(storage.total)}\n"
-        f"● ᴜsᴇᴅ ➥ {size_formatter(storage.used)}\n"
-        f"● ғʀᴇᴇ ➥ {size_formatter(storage.free)}\n"
-        f"● ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ ➥ {python_version}\n\n"
-        f"❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ sʜɪᴠᴀɴsʜ-xᴅ"
+        f"➪ᴜᴘᴛɪᴍᴇ: {uptime}\n"
+        f"➪ᴄᴘᴜ: {cpu}%\n"
+        f"➪ꜱᴛᴏʀᴀɢᴇ: {size_formatter(storage.total)} [ᴛᴏᴛᴀʟ]\n"
+        f"➪{size_formatter(storage.used)} [ᴜsᴇᴅ]\n"
+        f"➪{size_formatter(storage.free)} [ғʀᴇᴇ]\n"
+        f"➪ᴊᴀʀᴠɪs ᴠᴇʀsɪᴏɴ: {python_version}"
     )
-
     await message.reply(reply_text, quote=True)
 
-# Handle Forbidden Keywords
+@app.on_message(filters.command("auth") & filters.user(OWNER_ID))
+async def auth_user(_, message: Message):
+    if len(message.command) != 2:
+        await message.reply_text("Usage: /auth <user_id>")
+        return
 
-@app.on_message()
-async def handle_message(client, message):
-    if any(keyword in message.text for keyword in FORBIDDEN_KEYWORDS):
-        logging.info(f"Deleting message with ID {message.id}")
-        await message.delete()
-        await message.reply_text(f"@{message.from_user.username} 𝖣𝗈𝗇'𝗍 𝗌𝖾𝗇𝖽 𝗇𝖾𝗑𝗍 𝗍𝗂𝗆𝖾!")
-    elif any(keyword in message.caption for keyword in FORBIDDEN_KEYWORDS):
-        logging.info(f"Deleting message with ID {message.id}")
-        await message.delete()
-        await message.reply_text(f"@{message.from_user.username} 𝖣𝗈𝗇'𝗍 𝗌𝖾𝗇𝖽 𝗇𝖾𝗑𝗍 𝗍𝗂𝗆𝖾!")
-
-# Delete long edited messages but keep short messages and emoji reactions
-
-async def delete_long_edited_messages(client, edited_message: Message):
-    if edited_message.text:
-        if len(edited_message.text.split()) > 20:
-            await edited_message.delete()
+    user_id = int(message.command[1])
+    if user_id not in AUTHORIZED_USERS:
+        AUTHORIZED_USERS.append(user_id)
+        save_authorized_users(AUTHORIZED_USERS)
+        await message.reply_text(f"User {user_id} has been authorized.")
     else:
-        if edited_message.sticker or edited_message.animation or edited_message.emoji:
-            return
+        await message.reply_text(f"User {user_id} is already authorized.")
 
+@app.on_message(filters.command("unauth") & filters.user(OWNER_ID))
+async def unauth_user(_, message: Message):
+    if len(message.command) != 2:
+        await message.reply_text("Usage: /unauth <user_id>")
+        return
+
+    user_id = int(message.command[1])
+    if user_id in AUTHORIZED_USERS:
+        AUTHORIZED_USERS.remove(user_id)
+        save_authorized_users(AUTHORIZED_USERS)
+        await message.reply_text(f"User {user_id} has been unauthorized.")
+    else:
+        await message.reply_text(f"User {user_id} is not authorized.")
+
+@app.on_message(filters.command("listauth") & filters.user(OWNER_ID))
+async def list_authorized_users(_, message: Message):
+    if not AUTHORIZED_USERS:
+        await message.reply_text("No users are currently authorized.")
+        return
+
+    authorized_users = "\n".join(map(str, AUTHORIZED_USERS))
+    await message.reply_text(f"Authorized users:\n{authorized_users}")
+
+# Callback Query Handlers
+@app.on_callback_query(filters.regex("vip_back"))
+async def vip_back_callback_handler(_, query: CallbackQuery):
+    await query.message.edit_caption(caption=START_TEXT, reply_markup=InlineKeyboardMarkup(gd_buttons))
+
+@app.on_callback_query(filters.regex("back_to_start"))
+async def back_to_start_callback_handler(_, query: CallbackQuery):
+    await query.answer()
+    await query.message.delete()
+    await start_command_handler(_, query.message)
+
+# Message Handlers
 @app.on_edited_message(filters.group & ~filters.me)
-async def handle_edited_messages(_, edited_message: Message):
-    await delete_long_edited_messages(_, edited_message)
+async def delete_edited_message(client, message):
+    # Wait for a specified time (e.g., 2 seconds)
+    await asyncio.sleep(2)
+    await message.delete()
+    await message.reply(f"ʏᴏᴜ ᴇᴅɪᴛᴇᴅ ᴍᴇssᴀɢᴇ ɪs ᴛᴏᴏ ʟᴏɴɢ ᴛʜᴀᴛ's ᴡʜʏ ɪ ʜᴀᴠᴇ ᴅᴇʟᴇᴛᴇᴅ ɪᴛ.")
+        
+        
+# Command to add bad words
+@app.on_message(
+    filters.command(["badword"]))
+async def add_bad_word(client: Client, message: Message):
+    if len(message.command) < 2:
+        await message.reply("Please provide a bad word to add. Usage: /add_bad_word <word>")
+        return
 
-# Delete long messages in groups and reply with a warning
+    new_bad_word = message.command[1].lower()
+    if new_bad_word in bad_words:
+        await message.reply(f"{new_bad_word} is already in the list of bad words.")
+    else:
+        bad_words.add(new_bad_word)
+        await message.reply(f"{new_bad_word} has been added to the list of bad words.")
 
-MAX_MESSAGE_LENGTH = 25 # Define the maximum allowed length for a message
-
-async def delete_long_messages(client, message: Message):
-    if message.text:
-        if len(message.text.split()) > MAX_MESSAGE_LENGTH:
-            await message.delete()
-
-@app.on_message(filters.group & ~filters.me)
-async def handle_messages(_, message: Message):
-    await delete_long_messages(_, message)
-
-# -----------------------------------------------------------------------------------
-
-
-async def delete_pdf_files(client, message):
-    if message.document and message.document.mime_type == "application/pdf":
-        warning_message = f"⬤ ʜᴇʏ {user_mention} ᴅᴏɴ'ᴛ sᴇɴᴅ ᴘᴅғ ғɪʟᴇs ʙᴀʙʏ, ғᴏʀ ᴄᴏᴘʏʀɪɢʜᴛ ᴄʟɪᴍʙ."
-        await message.reply_text(warning_message)
+# Message handler to filter bad words
+@app.on_message(filters.text)
+async def bad_word_filter(client: Client, message: Message):
+    if any(bad_word in message.text.lower() for bad_word in bad_words):
         await message.delete()
-    else:  
-        pass
-
-@app.on_message(filters.group & filters.document)
-async def message_handler(client, message):
-    await delete_pdf_files(client, message)
+        await message.reply("Your message contained inappropriate language and has been removed.")
